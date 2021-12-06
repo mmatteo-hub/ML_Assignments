ebf7bd8175ace2419e4ebfe9574038fd88c026d0
Matteo Maragliano <S4636216@studenti.unige.it>
2021-12-07 00:37:40 +0100

    Added kNN Classifier

--- 3-kNN Classifier/kNN.m
+++ 3-kNN Classifier/kNN.m
@@ -1,44 +1,63 @@
 function [target, error_rate] = kNN(X_train,T_train,X_test,K,T_test)
-
+%% Checking dimensions
 if nargin() == 5
-    t = 1;
+    test_target_given = 1;
 elseif nargin() == 4
-    t = 0;
+    test_target_given = 0;
+else
+    disp("Incorrect number of inputs");
+    return
 end
 
+%% initialization
 [n,d] = size(X_train);
 [m,d1] = size(X_test);
 [n1,c] = size(T_train);
-if t == 1
+if test_target_given
     [m1,c1] = size(T_test);
 end
-%% Check dimensions and k
-if(d1 ~= d || K <= 0 || K > n)
-    error("Inputs not correct!");
+    
+if test_target_given
+    if n == n1 && m == m1 && d == d1 && c == c1 && c == 1
+    else
+        disp("Error");
+        return
+    end
+else
+    if n == n1 && d == d1 && c == 1
+    else
+        disp("Error");
+        return
+    end
 end
 
-target = zeros(m,1);
+if K <= 0
+    disp("k negative");
+elseif K > n
+    disp("k too big");
+end
 
-%% Classify the test according to the kNN rule
-for i=1:m
-    vec = zeros(K,1);
-    D = zeros(m,1);
-    for j=1:n
-        D(i,j) = pdist2(X_train(j,:),X_test(i,:));
+%% Building the classifier
+target = zeros(m,1);
+for i = 1:m
+    fdist = zeros(K,1);
+    errors = zeros(n,1);
+    for h = 1:n
+        errors(h,1) = norm(X_train(h,:)- X_test(i,:));
     end
-    [~,index_min] = mink(D,K);
-    for h=1:K
-        vec(h,1) = T_train(index_min(h,1),1);
+    [~, index] = mink(errors,K);
+    for k = 1:K
+        fdist(k,1) = T_train(index(k,1), 1);
     end
-    target(i,1) = mode(vec,1);
+    target(i,1) = mode(fdist,1);
 end
 error_rate = 0;
-if t
-    for a=1:m
-        if target(a,1) ~= T_test(a,1)
-            error_rate = error_rate + 1/length(target);
-        end
-    end
+if test_target_given
+   for l = 1:m
+       if T_test(l,1) ~= target(l,1)
+           error_rate = error_rate + 1 / length(target);
+       end
+   end
 end
 end
 
