ee5dcd10721c908d7c42e75d4a2fc8d2452e91ed
Matteo Maragliano <S4636216@studenti.unige.it>
2021-12-08 02:00:24 +0100

    Added kNN Classifier

--- 3-kNN Classifier/Classifier_kNN.m
+++ 3-kNN Classifier/Classifier_kNN.m
@@ -15,41 +15,35 @@ for cl=1:10
     T_train_cl = double(T_train == cl);
     T_test_cl = double(T_test == cl);
     
-    % take 5% of the data set
-    rows = randperm(60000,floor(0.025*60000));
+    % take % of the data set
+    rows = randperm(60000,floor(0.01*60000));
     X_train_sub = X_train(rows,:);
     T_train_sub = T_train(rows,:);
     
-    % take 10% of the data set
-    rows = randperm(10000,floor(0.10*10000));
+    % take % of the data set
+    rows = randperm(10000,floor(0.05*10000));
     X_test_sub = X_test(rows,:);
     T_test_sub = T_test(rows,:);
     
     
-    for i=1:length(K_array)
-        for j=1:length(K_array)
-            [target, err_matrix] = kNN(X_train_sub,T_train_sub,X_test_sub,K_array(j),T_test_sub);
-            accuracy(cl,i) = 1-err_matrix;
-        end
+    i=1;
+    for j=1:length(K_array)
+        [target, err_rate] = kNN(X_train_sub,T_train_sub,X_test_sub,K_array(j),T_test_sub);
+        accuracy(cl,i) = 1-err_rate;
+        i=i+1;
     end
 end
 
 figure;
 hold on;
 plot(K_array,accuracy(10,:),'DisplayName','Class 0', 'LineWidth', 2);
-plot(K_array,accuracy(1,:),'DisplayName','Class 1', 'LineWidth', 2);
-plot(K_array,accuracy(2,:),'DisplayName','Class 2', 'LineWidth', 2);
-plot(K_array,accuracy(3,:),'DisplayName','Class 3', 'LineWidth', 2);
-plot(K_array,accuracy(4,:),'DisplayName','Class 4', 'LineWidth', 2);
-plot(K_array,accuracy(5,:),'DisplayName','Class 5', 'LineWidth', 2);
-plot(K_array,accuracy(6,:),'DisplayName','Class 6', 'LineWidth', 2);
-plot(K_array,accuracy(7,:),'DisplayName','Class 7', 'LineWidth', 2);
-plot(K_array,accuracy(8,:),'DisplayName','Class 8', 'LineWidth', 2);
-plot(K_array,accuracy(9,:),'DisplayName','Class 9', 'LineWidth', 2);
+for i=1:9
+    plot(K_array,accuracy(i,:),'LineWidth', 2);
+end
 title('Accuracy');
 xlabel('K');
 ylabel('Accuracy in percentage');
+legend('Class 0','Class 1','Class 2','Class 3','Class 4','Class 5','Class 6','Class 7','Class 8','Class 9')
 grid;
 
 lgd = legend;
-
--- 3-kNN Classifier/kNN.m
+++ 3-kNN Classifier/kNN.m
@@ -1,63 +1,60 @@
-function [target, error_rate] = kNN(X_train,T_train,X_test,K,T_test)
-%% Checking dimensions
+function [target, error_rate] = kNN(X_train,T_train,X_test,k,T_test)
+%% Check dims
 if nargin() == 5
-    test_target_given = 1;
+    t = 1;
 elseif nargin() == 4
-    test_target_given = 0;
+    t = 0;
 else
     disp("Incorrect number of inputs");
     return
 end
 
-%% initialization
 [n,d] = size(X_train);
 [m,d1] = size(X_test);
 [n1,c] = size(T_train);
-if test_target_given
+if t
     [m1,c1] = size(T_test);
 end
-    
-if test_target_given
+
+if t
     if n == n1 && m == m1 && d == d1 && c == c1 && c == 1
     else
-        disp("Error");
+        disp("Incorrect number of column or rows");
         return
     end
 else
     if n == n1 && d == d1 && c == 1
     else
-        disp("Error");
+        disp("Incorrect number of column or rows");
         return
     end
 end
 
-if K <= 0
-    disp("k negative");
-elseif K > n
-    disp("k too big");
+if k <= 0
+    disp("Invalid k, must be k > 0");
+elseif k > n
+    disp("Invalid k, bigger than dataset");
 end
 
-%% Building the classifier
+%% Train
 target = zeros(m,1);
 for i = 1:m
-    fdist = zeros(K,1);
-    errors = zeros(n,1);
-    for h = 1:n
-        errors(h,1) = norm(X_train(h,:)- X_test(i,:));
+    labels = zeros(k,1);
+    distances = zeros(n,1);
+    for j = 1:n
+        distances(j,1) = norm(X_train(j,:)- X_test(i,:));
     end
-    [~, index] = mink(errors,K);
-    for k = 1:K
-        fdist(k,1) = T_train(index(k,1), 1);
+    [~, indexes] = mink(distances,k);
+    for h = 1:k
+        labels(h,1) = T_train(indexes(h,1), 1);
     end
-    target(i,1) = mode(fdist,1);
+    target(i,1) = mode(labels,1);
 end
 error_rate = 0;
-if test_target_given
+if t
    for l = 1:m
-       if T_test(l,1) ~= target(l,1)
-           error_rate = error_rate + 1 / length(target);
+       if target(l,1) ~= T_test(l,1)
+           error_rate = error_rate + 1/m;
        end
    end
 end
-end
-
