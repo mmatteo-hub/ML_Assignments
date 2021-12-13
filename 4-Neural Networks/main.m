f0f9c4424d09c2618c7eca461ca463287131bbcd
Matteo Maragliano <S4636216@studenti.unige.it>
2021-12-13 15:57:03 +0100

    Added Neural Networks

--- 4-Neural Networks/main.m
+++ 4-Neural Networks/main.m
@@ -5,18 +5,18 @@ addpath('./mnist');
 
 %% Task 2: Autoencoder
 
-[X, T] = loadMNIST(0,1:4);
+[X, T] = loadMNIST(0,1:3);
 
 % take 10% of the total
-rows = randperm(12700,floor(0.10*12700));
+rows = randperm(length(X(:,1)),floor(0.10*length(X(:,1))));
 X_sub = X(rows,:);
 T_sub = T(rows,:);
 
 nh = 2;
 myAutoencoder = trainAutoencoder(X_sub',nh);
-[Xenc, Tenc] = loadMNIST(0,1:4);
+[Xenc, Tenc] = loadMNIST(0,1:3);
 
-rows = randperm(12700,floor(0.10*12700));
+rows = randperm(length(X(:,1)),floor(0.10*length(X(:,1))));
 X_sub_enc = Xenc(rows,:);
 T_sub_enc = Tenc(rows,:);
 
@@ -26,5 +26,3 @@ myEncodedData = encode(myAutoencoder, X_sub_enc');
 addpath('./plot');
 figure
 plotcl(myEncodedData',T_sub_enc);
-%text(T_sub_enc(:,1),T_sub_enc(:,1),'');
-
